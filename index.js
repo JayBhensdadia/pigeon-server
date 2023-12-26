36409c789dbfd88b157ad814dd1ba8e4e43c50a7
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-12-26 17:24:40 +0530

    project setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5500;
+
+
+app.get("/",(req,res)=>{
+    res.send("Hi there");
+})
+
+
+app.listen(PORT,()=>{
+    console.log("server process running on port 5500");
+})
