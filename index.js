e72a5403f5042057f442c32f37f402d6e33e3ca5
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-12-26 19:15:34 +0530

    complete defining schemas for db

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5500;
+
+
+//connect to db
+const connectDb = async ()=>{
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log("server is connected to db");
+    } catch (error) {
+        console.log("Server is NOT connected to db ",error.message);
+    }
+}
+
+connectDb();
 
 //default route
 app.get("/",(req,res)=>{
     res.send("Hi there");
 })
 
-//create db schema
+
 
 app.listen(PORT,()=>{
     console.log("server process running on port 5500");
